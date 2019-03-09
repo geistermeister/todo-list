214d31ea2b3541dd0d019562f49d20a9243f7650
Geistermeister <dermitdemdingtanzt@hotmail.de>
2019-03-09 20:32:32 +0100

    UI/UX update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
     counter: -1
   }
 
-  handleOnAdd = () => {
+  handleOncssAdd = () => {
     if(this.state.input !== ''){
       this.setState({todos: [...this.state.todos, this.state.input]})
       this.setState({input: ''})
@@ -23,57 +23,70 @@ export default class App extends Component {
 s
   handleOnDelete = id => {
     this.setState({todos: this.state.todos.filter((_, index) => id !== index)})
-
   }
-
+  
   render() {
     return (
-      <div className={container}>
-        <div className={todoListContainer}>
+      <div className={cssContainer}>
+        <div>
           <h1>ToDo-List</h1>
+          <div className={cssNewToDoContainer}>
+            <input type='text' onChange={this.handleOnChange} value={this.state.input} className={cssNewToDo}></input>
+            <div className={cssAddContainer}>
+              <i className={`${cssAdd} material-icons`} onClick={this.handleOncssAdd}>add</i>
+            </div>
+          </div>
           {this.state.todos.map((item, index) => 
             <ToDo key={index} id={index} value={item} handleOnDelete={this.handleOnDelete}/>)
           }
-          <div className={newToDoContainerStyle}>
-            <input type='text' onChange={this.handleOnChange} value={this.state.input} className={newToDoStyle}></input>
-            <img src='./icons/baseline_add_circle_outline_black_48dp.png' alt='Add a new ToDo' className={addStyle} onClick={this.handleOnAdd}></img>
-          </div>
         </div> 
       </div>
     )
   }
 }
 
-const container = css`
+const cssContainer = css`
   display: flex;
   flex-direction: column;
   align-items: center;
   font-family: sans-serif;
 `
 
-const todoListContainer = css`
-  display: flex;
-  flex-direction: column;
-  flex-start: left;
-`
-
-const newToDoContainerStyle = css`
+const cssNewToDoContainer = css`
   display: flex;
   flex-direction: row;
   align-items: center;
-  margin-top: 10px;
+  padding: 5px;
+  margin-bottom: 10px;
 `
 
-const newToDoStyle = css`
+const cssNewToDo = css`
   height: 30px;
   width: 300px;
+  padding-left: 5px;
+  border: 1px solid #007777;
+  border-right: 0px;
+  &:focus{
+    outline: none;
+  }
 `
 
-const addStyle= css`
-  height: 30px;
-  width: 30px;
-  margin-left: 5px;
+const cssAdd = css`
+  line-height: 30px;
+  font-size: 30px;
+  color: white;
   &:hover {
     cursor: pointer;
+  }
+`
+
+const cssAddContainer = css`
+  display: flex;
+  align-itmes: center;
+  border: 2px solid #158e8e;
+  background: #158e8e;
+  &:hover {
+    background: #019e9e;
+    border: 2px solid #019e9e;
   }
 `
--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -4,43 +4,74 @@ import {css} from 'emotion'
 export default class ToDo extends Component {
 
   state = {
-    value: this.props.value
+    value: '',
+    showDelete: false
   }
 
   handleOnChange = item => {
     this.setState({value: item.target.value})
   }
 
+  handleOnMouseOver = () => {
+    this.setState({showDelete: true})
+  }
+
+  handleOnMouseOut = () => {
+    this.setState({showDelete: false})
+  }
+
   render() {
     return(
-      <div className={todoContainer}>
-        <div type='text' onChange={this.handleOnChange} className={todoStyle}>{this.props.value}</div>
-        <img src='./icons/baseline_delete_black_48dp.png' alt='Add' className={deleteStyle} onClick={() => this.props.handleOnDelete(this.props.id)}></img>
+      <div className={cssToDoContainer} 
+        onMouseOver={this.handleOnMouseOver}
+        onMouseOut={this.handleOnMouseOut}>
+        <div
+          className={cssToDo}
+          >
+          {this.props.value}
+        </div>
+        {<i 
+          className={`${this.state.showDelete ? cssDelete : cssDeleteHide} material-icons`} 
+          onClick={() => this.props.handleOnDelete(this.props.id)}>
+          delete
+        </i>}
       </div>
     )
   }
 }
-const todoStyle = css`
-  height: 30px;
+const cssToDo = css`
   width: 300px;
+  padding-left: 5px;
   line-height: 30px;
-  border-style: solid;
-  border-color: transparent;
-  border-bottom: 2px solid black;
+  color: white;
   user-select: none;
+  overflow-wrap: break-word;
 `
 
-const deleteStyle= css`
-  height: 30px;
-  width: 30px;
-  margin-left: 5px;
+const cssDelete = css`
+  font-size: 30px;
+  color: #007777;
   &:hover {
     cursor: pointer;
+    color: #006060;
   }
 `
 
-const todoContainer = css`
+const cssDeleteHide = css`
+  font-size: 30px; 
+  display: none;
+`
+
+const cssToDoContainer = css`
   display: flex;
   flex-direction: row;
   align-items: center;
+  padding: 5px;
+  user-select: none;
+  border-bottom: 1px solid white;
+  background: #158e8e;
+  &:hover {
+    background: #019e9e;
+  }
+  
 `
